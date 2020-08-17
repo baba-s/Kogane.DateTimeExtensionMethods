58213cda8e688e16c41276f9de9937687c508ea4
baba_s <shotababa1206@gmail.com>
2020-08-17 09:52:05 +0900

    first commit

---
+++ Scripts/DateTimeExt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Kogane
+{
+	/// <summary>
+	/// DateTime 型の拡張メソッドを管理するクラス
+	/// </summary>
+	public static class DateTimeExt
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// yyyy/MM/dd HH:mm:ss 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToPattern( this DateTime self )
+		{
+			return self.ToString( "yyyy/MM/dd HH:mm:ss" );
+		}
+
+		/// <summary>
+		/// yyyy/MM/dd 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToShortDatePattern( this DateTime self )
+		{
+			return self.ToString( "yyyy/MM/dd" );
+		}
+
+		/// <summary>
+		/// yyyy年M月d日 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToLongDatePattern( this DateTime self )
+		{
+			return self.ToString( "yyyy年M月d日" );
+		}
+
+		/// <summary>
+		/// yyyy年M月d日 HH:mm:ss 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToFullDateTimePattern( this DateTime self )
+		{
+			return self.ToString( "yyyy年M月d日 HH:mm:ss" );
+		}
+
+		/// <summary>
+		/// MM/dd HH:mm 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToMiddleDateTimePattern( this DateTime self )
+		{
+			return self.ToString( "MM/dd HH:mm" );
+		}
+
+		/// <summary>
+		/// HH:mm 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToShortTimePattern( this DateTime self )
+		{
+			return self.ToString( "HH:mm" );
+		}
+
+		/// <summary>
+		/// HH:mm:ss 形式の文字列に変換して返します
+		/// </summary>
+		public static string ToLongTimePattern( this DateTime self )
+		{
+			return self.ToString( "HH:mm:ss" );
+		}
+	}
+}
---
+++ Scripts/TimeSpanExt.cs
@@ -0,0 +1,69 @@
+﻿//using System;
+
+//namespace Kogane
+//{
+//	/// <summary>
+//	/// TimeSpan 型の拡張メソッドを管理するクラス
+//	/// </summary>
+//	public static class TimeSpanExt
+//	{
+//		//================================================================================
+//		// 関数(static)
+//		//================================================================================
+//		/// <summary>
+//		/// yyyy/MM/dd HH:mm:ss 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToPattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "yyyy/MM/dd HH:mm:ss" );
+//		}
+
+//		/// <summary>
+//		/// yyyy/MM/dd 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToShortDatePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "yyyy/MM/dd" );
+//		}
+
+//		/// <summary>
+//		/// yyyy年M月d日 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToLongDatePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "yyyy年M月d日" );
+//		}
+
+//		/// <summary>
+//		/// yyyy年M月d日 HH:mm:ss 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToFullDateTimePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "yyyy年M月d日 HH:mm:ss" );
+//		}
+
+//		/// <summary>
+//		/// MM/dd HH:mm 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToMiddleDateTimePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "MM/dd HH:mm" );
+//		}
+
+//		/// <summary>
+//		/// HH:mm 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToShortTimePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "HH:mm" );
+//		}
+
+//		/// <summary>
+//		/// HH:mm:ss 形式の文字列に変換して返します
+//		/// </summary>
+//		public static string ToLongTimePattern( this TimeSpan self )
+//		{
+//			return new DateTime( self.Ticks ).ToString( "HH:mm:ss" );
+//		}
+//	}
+//}
